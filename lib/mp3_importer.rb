d648701bd6eb7e21e9e83ee2aa2c3bec95b9bd59
Neil Empey <argus.two.2@gmail.com>
2020-04-29 22:30:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,17 @@
 class MP3Importer
 
-  def initialize(music_importer)
-    @music_importer = music_importer
+    attr_accessor :path
 
+  def initialize(path)
+      @path = path
   end
 
+  def files
+    @files ||= Dir.entries(@path).select {|song|      !File.directory?(song) && song.end_with?(".mp3")}
+  end
 
-
+  def import
+      files.each {|song| Song.new_by_filename(song)}
+  end
 
 end
