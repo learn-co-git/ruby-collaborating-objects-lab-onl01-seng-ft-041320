a34146169b321899739ffd7d6f53bca8d3d09e7b
Neil Empey <argus.two.2@gmail.com>
2020-04-29 22:31:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,24 @@
 require 'byebug'
 
 class Song
-  
-  attr_accessor :artist
-  attr_reader :name
-  @@all = []
+
+  attr_accessor :name, :artist
+
   def initialize(name)
-    @@all << self 
+    @name = name
   end
-  
-  def name= 
-    @song
-  end 
-  
-  def self.all
-    @@all 
-  end 
 
-  
-  def Song.new_by_filename(filename)
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
-    song = Song.new(filename.split(" - ")[1])
-    
-  end 
-  
-  
-  
-end 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+end
