e34b5b185b88906341633cb13d7a15d192085980
Neil Empey <argus.two.2@gmail.com>
2020-04-29 22:29:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,42 @@
 
 require "pry"
 require "byebug"
-class Artist 
-  
-  attr_accessor :name, :song
-  
-  @@all = []
-  @all = []
-  
+class Artist
+
+  attr_accessor :name, :songs
+      @@all =[]
+
   def initialize(name)
-    @name = name 
-    @@all << self
+    @name = name
     @songs = []
   end
-  
-  def self.all 
-    @@all 
-  end
-  
+
   def add_song(song)
-    @songs << song 
+    @songs << song
   end
-  
-  def songs 
-    Song.all.select {|song| song.artist == self}
-  end 
-  
-  
-  
-end 
+
+  def save
+      @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(artist_name)
+          artist = self.all.find {|artist| artist.name == artist_name}
+          if artist
+              artist
+              else
+                new_artist_name = self.new(artist_name)
+                new_artist_name.save
+                new_artist_name
+              end
+            end
+
+            def print_songs
+                @songs.each do |song|
+                  puts song.name
+                end
+              end
+end
