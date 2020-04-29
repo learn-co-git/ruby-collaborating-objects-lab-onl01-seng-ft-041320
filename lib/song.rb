bd1b170882a9a04cdd9cdc3db57d442d2dfbcada
Neil Empey <argus.two.2@gmail.com>
2020-04-29 22:37:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,13 @@ require 'byebug'
 
 class Song
 
+  @@all = []
+
   attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def self.new_by_filename(file_name)
