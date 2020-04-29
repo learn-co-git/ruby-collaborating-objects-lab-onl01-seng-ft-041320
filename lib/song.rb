eb79fd8b69540aa69048d38da464fcf89a97b9fe
Neil Empey <argus.two.2@gmail.com>
2020-04-29 22:38:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
+    new_song = Song.new(song)
     new_song.artist_name = artist
     new_song
   end
